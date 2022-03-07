249c88fed0ad61116da729e1b21a03402167a2a9
Rachita Choudhury <47971627+rachita26@users.noreply.github.com>
2022-03-07 15:53:47 +0530

    Initial commit

---
+++ src/components/TODO.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const Todo = () => {
+  const [todoList, setTodoList] = useState([]);
+  const [currentTodo, setCurrentTodo] = useState("");
+
+  const addTodo = () => {
+    setTodoList((prevTodoList) => [...prevTodoList, currentTodo]);
+    setCurrentTodo("");
+  };
+  return (
+    <>
+      <input
+        placeholder="todo"
+        onChange={(e) => setCurrentTodo(e.target.value)}
+        value={currentTodo}
+      />
+      <btn onClick={addTodo}>ADD</btn>
+      <br />
+      {todoList.map((todo) => (
+        <li>{todo}</li>
+      ))}
+    </>
+  );
+};
+
+export default Todo;
